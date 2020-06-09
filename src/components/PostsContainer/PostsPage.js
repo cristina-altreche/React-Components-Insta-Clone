ae42061a64360790b0ca4e9a9f4cecbc720628b6
Cristina Altreche <c.altreche@gmail.com>
2020-06-09 17:18:42 -0500

    comment section updated

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -14,7 +14,7 @@ const PostsPage = () => {
       {/* map through data here to return a Post and pass data as props to Post */}
       {userPosts.map((userObject) => {
         // console.log(userObject);
-        return <Post key={userObject.id} post={userObject} />;
+        return <Post key={userObject.id} post={userObject}/>;
       })}
     </div>
   );
